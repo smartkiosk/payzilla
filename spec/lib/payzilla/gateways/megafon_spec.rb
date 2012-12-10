3ca5e492a6ad1bb0a26475e3f87cb785ef571134
Boris Staal <boris@roundlake.ru>
2012-12-10 13:12:38 +0700

    Megafon specs fixed

--- spec/lib/payzilla/gateways/megafon_spec.rb
+++ spec/lib/payzilla/gateways/megafon_spec.rb
@@ -12,31 +12,34 @@ describe Payzilla::Gateways::Megafon do
     config.attachment_key  = File.new('certificates/megafon.key')
 
     @transport = Payzilla::Gateways::Megafon.new(config, './log/megafon.log')
+
+    @date     = DateTime.now
+    @payments = %w(9268123698)
+
+    @payments = @payments.each_with_index.map do |x,i|
+      OpenStruct.new(
+        :id => Time.now.to_i+i,
+        :account => x,
+        :enrolled_amount => 100,
+        :created_at => @date
+      )
+    end
+
+    @revision = OpenStruct.new(:id => Time.now.to_i, :payments => @payments, :date => @date.to_date)
   end
 
   it "checks" do
-    payment = OpenStruct.new(:account => '9268123698')
-    @transport.check(payment)[:success].should == true
+    @transport.check(@payments.first)[:success].should == true
 
     payment = OpenStruct.new(:account => '123')
     @transport.check(payment)[:success].should == false
   end
 
   it "pays" do
-    payment = OpenStruct.new(:id => Time.now.to_i, :account => '9268123698', :enrolled_amount => 100)
-    @transport.pay(payment)[:success].should == true
+    @transport.pay(@payments.first)[:success].should == true
   end
 
   it "revises" do
-    payments = []
-    date     = DateTime.now - 1
-
-    5.times do
-      payments << OpenStruct.new(:id => Time.now.to_i, :account => '9268123698', :paid_amount => 100, :enrolled_amount => 100, :created_at => date)
-    end
-
-    revision = OpenStruct.new(:payments => payments, :date => date)
-
-    @transport.revise(revision)[:success].should == true
+    @transport.revise(@revision)[:success].should == true
   end
 end
