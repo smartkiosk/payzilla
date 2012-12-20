1d67f1bccab8432d491efdfe803188d7fe66d678
Vasilij Melnychuk <isqrel@gmail.com>
2012-12-21 00:56:46 +0200

    attr_accessor for Payzilla::Payment

--- lib/payzilla/payment.rb
+++ lib/payzilla/payment.rb
@@ -3,5 +3,22 @@ module Payzilla
     def initialize(args={})
       args.each{|k,v| send "#{k}=", v}
     end
+
+    # Dynamic attr_accessor
+    def method_missing(k, *v)
+      k = k.to_s
+      setter = if k[-1] == "="
+                 k.slice! -1
+               else
+                 nil
+               end
+
+      unless setter.nil?
+        instance_variable_set("@#{k}".to_sym, v.first)
+      end
+
+      var = instance_variable_get("@#{k}".to_sym)
+      var.nil? ? super : var
+    end
   end
 end
