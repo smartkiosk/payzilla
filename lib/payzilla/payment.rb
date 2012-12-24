0341bde36d5bd9396fda7eb368ee27998b95269c
Vasilij Melnychuk <isqrel@gmail.com>
2012-12-24 23:20:19 +0200

    Add payment types

--- lib/payzilla/payment.rb
+++ lib/payzilla/payment.rb
@@ -3,9 +3,17 @@ module Payzilla
     attr_accessor :id, :account, :created_at, :enrolled_amount,
                   :paid_amount, :terminal_id, :gateway_provider_id,
                   :discount_card, :subagent_id, :agent_id, :fields,
-                  :commission_amount
+                  :commission_amount, :payment_type
+
+    TYPE_CASH         = 0
+    TYPE_INNER_CARD   = 1
+    TYPE_FOREIGN_CARD = 2
+    TYPE_IBANK        = 3
+    TYPE_ACCOUNT      = 4
 
     def initialize(args={})
+      payment_type = self::TYPE_CASH
+
       args.each{|k,v| send "#{k}=", v}
     end
   end
